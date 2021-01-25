472ecd81913b18d439f870f43ed9838db7853024
christian <cpinotossi@web.de>
2021-01-25 23:55:44 +0100

    add autsch page

--- routes/autsch.js
+++ routes/autsch.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-const fs = require('fs');
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render();
+  res.render('autsch');
 });
 
 module.exports = router;
