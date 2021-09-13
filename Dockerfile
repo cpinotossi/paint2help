f9569a1f037b409dcbe05a14f018b5307ef8fda8
cpinotossi <cpinotossi@web.de>
2021-09-13 23:01:10 +0200

    add further docker settings

---
+++ Dockerfile
@@ -0,0 +1,38 @@
+FROM node:12-alpine
+
+ARG IMAGE_CREATE_DATE
+ARG IMAGE_VERSION
+ARG IMAGE_SOURCE_REVISION
+
+ENV PORT=80 \
+    CONFIG_FILES_PATH="/secrets"
+
+# Metadata as defined in OCI image spec annotations - https://github.com/opencontainers/image-spec/blob/master/annotations.md
+LABEL org.opencontainers.image.title="Paint2Help Node.js Express Web App" \
+      org.opencontainers.image.description="Simple Web App to show images." \
+      org.opencontainers.image.created=$IMAGE_CREATE_DATE \
+      org.opencontainers.image.version=$IMAGE_VERSION \
+      org.opencontainers.image.authors="cpinotossi@web.de" \
+      org.opencontainers.image.url="https://raw.githubusercontent.com/cpinotossi/paint2help/aks/Dockerfile" \
+      org.opencontainers.image.documentation="https://raw.githubusercontent.com/cpinotossi/paint2help/aks/README.md" \
+      org.opencontainers.image.vendor="cpinotossi@web.de" \
+      org.opencontainers.image.licenses="MIT" \
+      org.opencontainers.image.source="https://github.com/cpinotossi/paint2help.git" \
+      org.opencontainers.image.revision=$IMAGE_SOURCE_REVISION 
+
+# add debugging utilities
+RUN apk --no-cache add \
+  curl \
+  ca-certificates \
+  jq \
+  less \
+  vim
+
+# bundle app and install dependencies 
+COPY . /app
+WORKDIR /app
+RUN npm install
+
+# run application
+EXPOSE $PORT
+CMD npm start
--- app.js
+++ app.js
@@ -3,11 +3,6 @@ const morgan = require('morgan');
 var path = require('path');
 let cors = require('cors');
 
-
-var hwRouter = require('./routes/hw');
-var csaiRouter = require('./routes/csai');
-var stestRouter = require('./routes/stest');
-var aiRouter = require('./routes/ai');
 var userRouter = require('./routes/user');
 var app = express();
 
@@ -17,7 +12,7 @@ app.disable('view cache');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-//app.set('view options', { layout: 'layout' });
+app.set('view options', { layout: 'layout' });
 
 app.use(cors());
 app.use(morgan('tiny'));
@@ -30,20 +25,7 @@ let staticOptions = {
 
 app.use(express.static(path.join(__dirname, 'public'),staticOptions));
 
-//Call a simple Hello World Page
-app.use('/helloworld', hwRouter);
-//Call server side AI and simple Hello World Page
-//app.use('/hellossai', aiRouter, hwRouter);
-//Call client side AI and simple Hello World Page
-//app.use('/hellocsai', csaiRouter);
-//Call server and client side AI and simple Hello World Page
-//app.use('/hellocsssai', aiRouter, csaiRouter);
-
-
-//app.use('/autsch', aiRouter,stestRouter);
-//app.use('/uff', aiRouter,stestRouter);
-app.use('/user/*', aiRouter,userRouter);
-app.use('/', aiRouter,userRouter);
-
+app.use('/user/*', userRouter);
+app.use('/', userRouter);
 
 module.exports = app;
